54ed95c0f9fe1d77a61777c1b295feebfe88c24c
shijun18 <46990068+shijun18@users.noreply.github.com>
2023-04-12 20:35:27 +0800

    update readme and run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=
-python main_cpu.py --model ./configs/gpt3_test.json --steps_per_checkpoint 500 --new --task $1 --index $2
+python main_cpu.py --model ./configs/gpt3_test.json --steps_per_checkpoint 50 --new --task $1 --index $2
